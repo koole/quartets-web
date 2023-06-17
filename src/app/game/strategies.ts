2aba7a58e0c91fbffc6f2beb7163c4bbcd320eeb
Ioana - Andreea Cojocaru <104626723+CIA41998@users.noreply.github.com>
2023-06-17 17:48:57 +0200

    if no cards in hand, asks for random

--- src/app/game/strategies.ts
+++ src/app/game/strategies.ts
@@ -27,49 +27,57 @@ function getMostCards(
 ): [AgentType, Card] {
   const colorCounts: Map<string, number> = new Map();
   const numberSet: Set<number> = new Set();
-
-  // Count the occurrences of each color and collect numbers in the hand
-  cards.forEach((card) => {
-    const color = card.color;
-    const number = card.number;
-
-    colorCounts.set(color, (colorCounts.get(color) || 0) + 1);
-    numberSet.add(number);
-  });
-
-  // Find the color with the highest count
-  let mostCommonColor: string | undefined;
-  let highestCount = 0;
-
-  colorCounts.forEach((count, color) => {
-    if (count > highestCount) {
-      highestCount = count;
-      mostCommonColor = color;
-    }
-  });
-
-  // Collect numbers present in the most common color
-  const numbersInMostCommonColor: Set<number> = new Set();
-  cards.forEach((card) => {
-    if (card.color === mostCommonColor) {
-      numbersInMostCommonColor.add(card.number);
-    }
-  });
-
-  // Filter cards by the most common color and exclude numbers present in that color
-  const cardsWithMostCommonColor = CARD_LIST.filter(
-    (card) => card.color === mostCommonColor && !numbersInMostCommonColor.has(card.number)
-  );
-
-  // Randomly select a card from the filtered cards
   let randomCard: Card | undefined;
-  if (cardsWithMostCommonColor.length > 0) {
-    randomCard =
-      cardsWithMostCommonColor[
-        Math.floor(Math.random() * cardsWithMostCommonColor.length)
-      ];
+
+  if (cards.length === 0) {
+    console.log(cards.length)
+    // If the current player has no cards, select a random card from all available cards
+    randomCard = CARD_LIST[Math.floor(Math.random() * CARD_LIST.length)];
   }
 
+  else {
+    // Count the occurrences of each color and collect numbers in the hand
+    cards.forEach((card) => {
+      const color = card.color;
+      const number = card.number;
+
+      colorCounts.set(color, (colorCounts.get(color) || 0) + 1);
+      numberSet.add(number);
+    });
+
+    // Find the color with the highest count
+    let mostCommonColor: string | undefined;
+    let highestCount = 0;
+
+    colorCounts.forEach((count, color) => {
+      if (count > highestCount) {
+        highestCount = count;
+        mostCommonColor = color;
+      }
+    });
+
+    // Collect numbers present in the most common color
+    const numbersInMostCommonColor: Set<number> = new Set();
+    cards.forEach((card) => {
+      if (card.color === mostCommonColor) {
+        numbersInMostCommonColor.add(card.number);
+      }
+    });
+
+    // Filter cards by the most common color and exclude numbers present in that color
+    const cardsWithMostCommonColor = CARD_LIST.filter(
+      (card) => card.color === mostCommonColor && !numbersInMostCommonColor.has(card.number)
+    );
+
+    // Randomly select a card from the filtered cards
+    if (cardsWithMostCommonColor.length > 0) {
+      randomCard =
+        cardsWithMostCommonColor[
+          Math.floor(Math.random() * cardsWithMostCommonColor.length)
+        ];
+    }
+  }
+  
   // Randomly select an agent
   const otherAgents = agents.filter((agent) => agent !== currentAgent);
   const randomAgent =

af7bdf0fcd87172f3c809ef2074787ead3775ce8
Leon Koole <leon@koole.io>
2023-06-19 12:09:21 +0200

    Update game rules

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -13,9 +13,10 @@ export default function Home() {
             Game Rules
           </h1>
           <p className="mt-6 text-xl leading-8 text-justify">
-            Quartets is a dedicated deck card game consisting of 32 cards, divided into eight suites of four cards each.
-            In the case of our implementation, we chose to use a simplified version of the game, with 6 different colours 
-            for the cards, namely:
+            Quartets is a dedicated deck card game consisting of 32 cards,
+            divided into eight suites of four cards each. In the case of our
+            implementation, we chose to use a simplified version of the game,
+            with 6 different colours for the cards, namely:
             <ul className="flex gap-3 my-3">
               <li>
                 <span className="inline-block w-4 h-4 mr-2 rounded bg-red-500"></span>
@@ -44,30 +45,68 @@ export default function Home() {
             </ul>
           </p>
           <p className="mt-6 text-justify">
-            The game is played with three or more players, and the objective is to collect as many full suites of four cards
-            as possible. In the current game, there are 3 players out of which two of them are the automated agents following
-            a certain strategy. These strategies are further discussed on the Experiment page.
+            The game is played with three or more players, and the objective is
+            to collect as many full suites of four cards as possible. In the
+            current game, there are 3 players out of which two of them are the
+            automated agents following a certain strategy. These strategies are
+            further discussed on the Experiment page.
           </p>
           <p className="mt-6 text-justify">
-            At the beginning of the game, the deck of cards is shuffled, and each player is dealt an equal number of cards. 
-            Players should keep their cards hidden from other players. To start the game, a player is randomly chosen to make the first move. 
-            They can ask another player for a specific card from a suite, provided that they already have a card from that suite in their hand. Players can not ask for a card that they already have in their hand.
+            At the beginning of the game, the deck of cards is shuffled, and
+            each player is dealt an equal number of cards. Players should keep
+            their cards hidden from other players. To start the game, a player
+            is randomly chosen to make the first move.
           </p>
+          <ol className="list-decimal pl-8">
+            <li className="mt-6 text-justify">
+              Players can ask another player for a specific card from a suite,
+              provided that they already have a card from that suite in their
+              hand.
+            </li>
+            <li className="my-2">
+              Players can not ask for a card that they already have in their
+              hand.
+            </li>
+            <li className="my-2">
+              If a player has no more cards, they can ask for any card they want.
+            </li>
+            <li className="my-2">
+              If the player being asked has the requested card, they must give
+              it to the asking player, who then gets another turn to ask for a
+              card.
+            </li>
+            <li className="my-2">
+              If the player being asked does not have the requested card, it
+              becomes their turn to ask for a card.
+            </li>
+            <li className="my-2">
+              Whenever a player collects a full suite of four cards, known as a
+              &quot;Quartet,&quot; they must place the suite face-up in front of
+              them.
+            </li>
+            <li className="my-2">
+              The game continues until all the quartets have been formed. Once
+              all the quartets have been created, the game ends, and the player
+              with the most quartets is declared the winner.
+            </li>
+          </ol>
           <p className="mt-6 text-justify">
-            For example, if a player has a blue card in their hand with the number 1, then they can ask another player
-            for the card blue 2.
-            
-            
-            
-            If the player being asked has the requested card, they must give it to the asking player, who then gets another turn to ask for a card. If the player being asked does not have the requested card, it becomes their turn to ask for a card.
-            Whenever a player collects a full suite of four cards, known as a &quot;Quartet,&quot; they must place the suite face-up in front of them. The game continues until all the quartets have been formed. Once all the quartets have been created, the game ends, and the player with the most quartets is declared the winner.
-            It&apos;s important to note that Quartets has many variations to its rules, so the aforementioned rules are our interpretation of the game.
+            For example, if a player has a blue card in their hand with the
+            number 1, then they can ask another player for the card blue 2. If
+            the player being asked has the requested card, they must give it to
+            the asking player, who then gets another turn to ask for a card. If
+            the player being asked does not have the requested card, it becomes
+            their turn to ask for a card. Whenever a player collects a full
+            suite of four cards, known as a &quot;Quartet,&quot; they must place
+            the suite face-up in front of them. The game continues until all the
+            quartets have been formed. Once all the quartets have been created,
+            the game ends, and the player with the most quartets is declared the
+            winner. It&apos;s important to note that Quartets has many
+            variations to its rules, so the aforementioned rules are our
+            interpretation of the game.
           </p>
         </div>
       </div>
-
-
-
     </main>
-  )
+  );
 }

015903847b93667a692684201058b593c98a5844
Yorick Juffer <oyjuffer@gmail.com>
2023-06-24 11:41:54 +0200

    broken formal model

--- src/app/model/page.tsx
+++ src/app/model/page.tsx
@@ -32,6 +32,14 @@ export default function Home() {
             to be modelled increased significantly. Moreover, once a move is
             made throughout the game, the whole model had to be updated
           </p>
+          <h2>Formal Model</h2>
+          <p>This project uses the following definitions in reference to the set of <strong>m</strong> agents <strong>A = &langle; a<sub>1</sub>, a<sub>2</sub>, ..., a<sub>m</sub> &rangle;</strong> and the set of propositional atoms <strong>P</strong>. The Kripke model <strong>M</strong> for quartets consists of the tuple &langle; <strong>S</strong>, <strong>&pi;</strong>, <strong>R<sub>1</sub></strong>, ..., <strong>R<sub>m</sub></strong> &rangle; where:</p>
+
+          <ul>
+            <li><strong>Set of States (S):</strong> The set <strong>S</strong> represents all possible states of the system. In this case, each state <em>s</em> represents a particular configuration of cards in each agent's hand.</li>
+            <li><strong>Truth Assignment Function (&pi;):</strong> The function &pi;: S &rarr; (P &rarr; {<strong>t</strong>, <strong>f</strong>}) assigns truth values to propositional atoms for each state.</li>
+            <li><strong>Accessibility Relations (R<sub>1</sub>, R<sub>2</sub>, ..., R<sub>m</sub>):</strong> The accessibility relations R<sub>i</sub> &sube; S &times; S represent the relations between different states in the model. These relations define the transitions or possible state changes within the system.</li>
+          </ul>
           <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
             Common knowledge
           </h2>
@@ -99,7 +107,7 @@ export default function Home() {
               In order to be able to represent the knowledge in the game and the possible states, we make use of a
               simplified model.
               <br />
-              The true state, shown by the green dot <span className="inline-block w-3 h-3 bg-green-500 rounded-full"/> in each Kripke model,
+              The true state, shown by the green dot <span className="inline-block w-3 h-3 bg-green-500 rounded-full" /> in each Kripke model,
               is as follows:
               <ul className="flex justify-around mt-6 mb-12">
                 <li>
@@ -149,19 +157,19 @@ export default function Home() {
             <h2 className="mt-2 text-2xl font-bold tracking-tight text-gray-900">
               Inital state
             </h2>
-            This represents the start of the game, when the cards have just been dealt between the three players. 
-            Nobody has seen their cards, so anything is possible. 
+            This represents the start of the game, when the cards have just been dealt between the three players.
+            Nobody has seen their cards, so anything is possible.
             <KripkeModelHidden />
             <h2 className="mt-24 border-t border-gray-300 pt-24 text-2xl font-bold tracking-tight text-gray-900">
               Looking at cards
             </h2>
-            Players have seen their own cards, but not the other players' cards. 
+            Players have seen their own cards, but not the other players' cards.
             <br />
             In this scenario, we have the following true state:
             <br />
             <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
-              <b>True state</b>
+                <b>True state</b>
                 <li>
                   <strong>Player 1: </strong>
                   <br />
@@ -218,7 +226,7 @@ export default function Home() {
             <br />
             <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
-              <b>True state</b>
+                <b>True state</b>
                 <li>
                   <strong>Player 1: </strong>
                   <br />
@@ -263,32 +271,32 @@ export default function Home() {
                 </li>
               </ul>
             </div>
-            As we can see above in the true state, Player 2 does not have the card Green 1. Because of this, there is certain common 
+            As we can see above in the true state, Player 2 does not have the card Green 1. Because of this, there is certain common
             knowledge in the game, as follows:
             <br />
             <ul role="list" className="mt-8 max-w-xl space-y-2 text-gray-600">
-            <li className="flex gap-x-3">
-              <XCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 2 does not have Green 1 </span>
-            </li>
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 3 must have Green 1 </span>
-            </li>
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 1 must have Green 0 </span>
-            </li>
-          </ul>
+              <li className="flex gap-x-3">
+                <XCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 2 does not have Green 1 </span>
+              </li>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 3 must have Green 1 </span>
+              </li>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 1 must have Green 0 </span>
+              </li>
+            </ul>
             {/* - Player 2 does not have Green 1<br />
             - Player 3 must have Green 1<br />
             - Player 1 must have Green 0 <br /> */}
@@ -298,9 +306,9 @@ export default function Home() {
                   <strong>Player 1: </strong>
                   <br />
                   <div className="flex gap-3">
-                    <CardDisplay 
-                      hide={true} 
-                      card={{ color: 'red', number: 0, id: 'red-0' }} 
+                    <CardDisplay
+                      hide={true}
+                      card={{ color: 'red', number: 0, id: 'red-0' }}
                     />
                     <CardDisplay
                       hide={false}
@@ -312,9 +320,9 @@ export default function Home() {
                   <strong>Player 2: </strong>
                   <br />
                   <div className="flex gap-3">
-                  <div className="card-display-container" style={{ position: 'relative' }}>
-                    <CardDisplay x={true} hide={false} card={{ color: 'green', number: 1, id: 'red-0' }} />
-                  </div>
+                    <div className="card-display-container" style={{ position: 'relative' }}>
+                      <CardDisplay x={true} hide={false} card={{ color: 'green', number: 1, id: 'red-0' }} />
+                    </div>
                     <CardDisplay
                       hide={true}
                       card={{ color: "yellow", number: 0, id: "yellow-0" }}
@@ -326,8 +334,8 @@ export default function Home() {
                   <br />
                   <div className="flex gap-3">
                     <CardDisplay
-                        hide={false}
-                        card={{ color: "green", number: 1, id: "yellow-1" }}
+                      hide={false}
+                      card={{ color: "green", number: 1, id: "yellow-1" }}
                     />
                     <CardDisplay
                       hide={true}
@@ -347,7 +355,7 @@ export default function Home() {
             <br />
             <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
-              <b>True state</b>
+                <b>True state</b>
                 <li>
                   <strong>Player 1: </strong>
                   <br />
@@ -392,48 +400,48 @@ export default function Home() {
                 </li>
               </ul>
             </div>
-            As we can see above in the true state, Player 1 does not have the card Yelllow 1. Because of this, the common 
+            As we can see above in the true state, Player 1 does not have the card Yelllow 1. Because of this, the common
             knowledge gets updated with the following:
             <br />
             <ul role="list" className="mt-8 max-w-xl space-y-2 text-gray-600">
-            <li className="flex gap-x-3">
-              <XCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 1 does not have Yellow 1 </span>
-            </li>
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 2 must have Yellow 0 </span>
-            </li>
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 3 must have Yellow 1 </span>
-            </li>
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Players 1 and 2 both have a red card </span>
-            </li>
-          </ul>
+              <li className="flex gap-x-3">
+                <XCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 1 does not have Yellow 1 </span>
+              </li>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 2 must have Yellow 0 </span>
+              </li>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 3 must have Yellow 1 </span>
+              </li>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Players 1 and 2 both have a red card </span>
+              </li>
+            </ul>
             <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
                 <li>
                   <strong>Player 1: </strong>
                   <br />
                   <div className="flex gap-3">
-                  <div className="card-display-container" style={{ position: 'relative' }}>
-                    <CardDisplay x={true} hide={false} card={{ color: 'yellow', number: 1, id: 'yellow-1' }} />
-                  </div>
+                    <div className="card-display-container" style={{ position: 'relative' }}>
+                      <CardDisplay x={true} hide={false} card={{ color: 'yellow', number: 1, id: 'yellow-1' }} />
+                    </div>
                     <CardDisplay
                       hide={false}
                       card={{ color: "green", number: 0, id: "green-0" }}
@@ -445,9 +453,9 @@ export default function Home() {
                   <strong>Player 2: </strong>
                   <br />
                   <div className="flex gap-3">
-                  <div className="card-display-container" style={{ position: 'relative' }}>
-                    <CardDisplay x={true} hide={false} card={{ color: 'green', number: 1, id: 'red-0' }} />
-                  </div>
+                    <div className="card-display-container" style={{ position: 'relative' }}>
+                      <CardDisplay x={true} hide={false} card={{ color: 'green', number: 1, id: 'red-0' }} />
+                    </div>
                     <CardDisplay
                       hide={false}
                       card={{ color: "yellow", number: 0, id: "yellow-0" }}
@@ -460,8 +468,8 @@ export default function Home() {
                   <br />
                   <div className="flex gap-3">
                     <CardDisplay
-                        hide={false}
-                        card={{ color: "green", number: 1, id: "yellow-1" }}
+                      hide={false}
+                      card={{ color: "green", number: 1, id: "yellow-1" }}
                     />
                     <CardDisplay
                       hide={false}
@@ -480,12 +488,12 @@ export default function Home() {
             <h2 className="mt-24 border-t border-gray-300 pt-24 text-2xl font-bold tracking-tight text-gray-900">
               Round 3
             </h2>
-            Player 2 was not successful, so now the turn goes back to Player 1. Player 1 now asks Player 3 for 
+            Player 2 was not successful, so now the turn goes back to Player 1. Player 1 now asks Player 3 for
             Green 1. As Player 3 has Green 1 in its hand, Player 1 receives the card, resulting in Player 1
             having a Green Suite. This results in the true state: <br />
             <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
-              <b>True state</b>
+                <b>True state</b>
                 <li>
                   <strong>Player 1: </strong>
                   <br />
@@ -533,30 +541,30 @@ export default function Home() {
             Because of this move, the common knowledge is updated accordingly:
             <br />
             <ul role="list" className="mt-8 max-w-xl space-y-2 text-gray-600">
-            <li className="flex gap-x-3">
-              <CheckCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> Player 1 has a Green suite </span>
-            </li>
-            <li className="flex gap-x-3">
-              <XCircleIcon
-                className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              <span> No player has a Green card in their hand anymore </span>
-            </li>
-          </ul>
-          <div>
+              <li className="flex gap-x-3">
+                <CheckCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> Player 1 has a Green suite </span>
+              </li>
+              <li className="flex gap-x-3">
+                <XCircleIcon
+                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
+                  aria-hidden="true"
+                />
+                <span> No player has a Green card in their hand anymore </span>
+              </li>
+            </ul>
+            <div>
               <ul className="flex justify-around mt-6 mb-12 rounded-md bg-slate-100 py-12">
                 <li>
                   <strong>Player 1: </strong>
                   <br />
                   <div className="flex gap-3">
-                  <div className="card-display-container" style={{ position: 'relative' }}>
-                    <CardDisplay x={true} hide={false} card={{ color: 'yellow', number: 1, id: 'yellow-1' }} />
-                  </div>
+                    <div className="card-display-container" style={{ position: 'relative' }}>
+                      <CardDisplay x={true} hide={false} card={{ color: 'yellow', number: 1, id: 'yellow-1' }} />
+                    </div>
                     <SuitDisplay key="green" suit="green" />
                     <EmptyCard key="red" suit="red" />
                   </div>
@@ -596,7 +604,7 @@ export default function Home() {
               Round 4
             </h2>
             In the previous round, Player 1 was able to get the requested card, meaning that Player 1 can choose
-            another card. Therefore, Player 1 asks Player 2 for Red 1. This results in Player 1 receiving the card 
+            another card. Therefore, Player 1 asks Player 2 for Red 1. This results in Player 1 receiving the card
             Red 1, completing the Red suite. This results in the situation: <br />
 
             <div>

8abcb98080cb75855b98e893163da196965c0f04
Yorick Juffer <oyjuffer@gmail.com>
2023-06-24 18:36:50 +0200

    added draft of the discussion

--- src/app/discussion/page.tsx
+++ src/app/discussion/page.tsx
@@ -29,7 +29,16 @@ export default function Home() {
             Discussion
           </h1>
           <p>
-            placeholder
+            This project sought to simulate the game of Quartets using a Kripke model as a foundation for agent strategies. The objective was to evaluate the performance of four agent strategies: random, firstOrder, secondOrder, and Combined. The experiment involved running all possible combinations of these strategies against each other. The findings revealed that leveraging first or second-order knowledge generally enhanced an agent's performance, with first-order knowledge yielding the highest performance. However, the Combined strategy, which incorporated both first and second-order knowledge, did not demonstrate an equivalent level of success.
+          </p>
+          <p>
+            Some discussion points can be taken from these results. Firstly, the experiment demonstrated the effectiveness of utilizing first and second-order knowledge in improving agent performance. Both the firstOrder and secondOrder strategies consistently outperformed the random strategy, indicating that incorporating knowledge about the game and opponents' behaviour can lead to more successful outcomes. This highlights the importance of strategic decision-making based on observed patterns and information.
+          </p>
+          <p>
+            Secondly, the finding that the firstOrder strategy generally outperformed the secondOrder strategy suggests that focusing on immediate, first-order knowledge may be more impactful in Quartets gameplay. The success that it did find, was against combinations that didn't include the firstOrder strategy, indicating that the use of first-order knowledge may be disruptive against an agent that uses second-order knowledge.
+          </p>
+          <p>
+            However, it is notable that the Combined strategy, which combined both first and second-order knowledge, did not demonstrate a corresponding improvement in performance as expected. Despite achieving moderate success against random strategies and the random/Combined combination, the Combined strategy's overall performance remained relatively modest compared to the individual strategies.
           </p>
           <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             Limitations
